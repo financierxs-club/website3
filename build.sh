557e0449b03b1041631336a96191a71c00445194
Javier Jaimes <javier@lolvi.co>
2024-11-21 19:12:15 -0500

    Fix build with exit 0 & deno run in background...

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -fsSL https://deno.land/install.sh | sh
 export PATH="$HOME/.deno/bin:$PATH"
-deno run --allow-net --allow-read server.ts
+deno run --allow-net --allow-read server.ts & 
 exit 0
